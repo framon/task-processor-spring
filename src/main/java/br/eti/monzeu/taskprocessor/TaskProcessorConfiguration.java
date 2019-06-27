a68a29247c8b0158209ea45e8de6460ff1fe1b6c
Fábio Ramon <framon@monzeu.eti.br>
2019-06-26 22:42:57 -0300

    Add Spring configuration class

---
+++ src/main/java/br/eti/monzeu/taskprocessor/TaskProcessorConfiguration.java
@@ -0,0 +1,10 @@
+package br.eti.monzeu.taskprocessor;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class TaskProcessorConfiguration {
+
+}
